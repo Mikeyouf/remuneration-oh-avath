6b6e0b93fab3e9ec09769babbf9858d42ee0aef9
Antoine Mikael <33396875+Mikeyouf@users.noreply.github.com>
2018-11-13 09:08:05 +0100

    first commit

---
+++ build/precache-manifest.48d5b14b3c47f1b278c9dad97041066c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e1a339c4545ccaaa47e",
+    "url": "/static/js/main.2e1a339c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2e1a339c4545ccaaa47e",
+    "url": "/static/css/main.1d928ce7.chunk.css"
+  },
+  {
+    "revision": "4f0081f224a68f4ba8fde46d8d08f3f3",
+    "url": "/index.html"
+  }
+];
